5113bc5bf33a734c996fbd7d4e3b62ebe202815d
vivz <vivian-keke@hotmail.com>
2016-05-31 18:56:59 -0700

    typo

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ $(function() {
 		$.getJSON( "https://spreadsheets.google.com/feeds/list/1npkTdmKUovft7cpO7FVTeNXSc0jgsclscuifmIFDfaY/od6/public/values?alt=json", function (data) {	
 			$.each(data.feed.entry, function(i,entry) {
 			var code ='';	
-			code +='<div class="name"><b>'+entry.gsx$name.$t+'</b></div>';
-			code +='<div class="award">'+entry.gsx$award.$t+'</div>';
+			code +='<div class="award"><b>'+entry.gsx$award.$t+'</b></div>';
+			code +='<div class="name">'+entry.gsx$name.$t+'</div>';
 			code += '<div class="picture"> <img src="'+ entry.gsx$picture.$t + '"/></div>' ;
 			console.log(code);
 			$('div#content').append(code); 
