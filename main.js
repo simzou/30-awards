fa036434383b30d046bf51db4f075fb8f8567090
omniaura <omniaura5@gmail.com>
2016-05-31 20:00:41 -0700

    styling

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ $(function() {
 		$.getJSON( "https://spreadsheets.google.com/feeds/list/1npkTdmKUovft7cpO7FVTeNXSc0jgsclscuifmIFDfaY/od6/public/values?alt=json", function (data) {	
 			$.each(data.feed.entry, function(i,entry) {
 			var code ='';	
-			code +='<div class="award"><b>'+entry.gsx$award.$t+'</b></div>';
-			code +='<div class="name">'+entry.gsx$name.$t+'</div>';
+			code +='<h1>'+entry.gsx$award.$t+'</h1>';
+			code +='<h2>'+entry.gsx$name.$t+'</h2>';
 			code += '<div class="picture"> <img src="'+ entry.gsx$picture.$t + '"/></div>' ;
 			console.log(code);
 			$('div#content').append(code); 
